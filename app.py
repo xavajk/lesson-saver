48dc35ea9064f01e97f10a2b02dbc5736b8c8245
xavajk <xjkuehn@gmail.com>
2023-09-05 13:07:00 -0700

    added user text processing w/ vector store and embeddings

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 
 def get_pdf_text(pdf_docs):
     text = ''
@@ -10,8 +13,23 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
-def get_text_chunks(raw_text):
-    
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator='\n',
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    # OpenAI
+    embeddings = OpenAIEmbeddings()
+    # Instructor
+    # embeddings = HuggingFaceInstructEmbeddings(model_name='hkunlp/instructor-xl')
+    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vector_store
 
 def main():
     # initialize app
@@ -31,11 +49,10 @@ def main():
             with st.spinner("Processing..."):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-
                 # get text chunks
                 text_chunks = get_text_chunks(raw_text)
-
                 # create vector store w/ embeddings
+                vector_store = get_vector_store(text_chunks)
 
 if __name__ == '__main__':
     main()
